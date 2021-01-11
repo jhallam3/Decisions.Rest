dff6ef2e01d6370f896a7e5fcd048dfdb9a16857
Jon <Jon@WIN-B2L934R07O3>
2021-01-11 16:09:32 +0000

    init

---
+++ Decisions.Rest/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            List<KeyValuePair<string, string>> kvpHeaders = new List<KeyValuePair<string, string>>();
+            kvpHeaders.Add(new KeyValuePair<string, string>("Content-Type", "text/plain"));
+            kvpHeaders.Add(new KeyValuePair<string, string>("AuthKey", "1459ed78-3f8e-416c-94d4-2efefbf289f2"));
+
+            var sut = new Decisions.Rest.RestFunction().HttpRestRequestor(HttpMethod.Post, "http://192.168.2.10:19312", "/EngineStatus", kvpHeaders, null); 
+        }
+    }
+}
---
+++ Decisions.Rest/Decisions.Rest/RestFunction.cs
@@ -0,0 +1,35 @@
+﻿using DecisionsFramework.Design.Flow;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Decisions.Rest
+{
+    [AutoRegisterMethodsOnClass(true, "Integration/Rest Services")]
+    public class RestFunction
+    {
+        
+        public string HttpRestRequestor(HttpMethod method, string BaseAddress, string RequestUri, List<KeyValuePair<string,string>> Headers, List<KeyValuePair<string,string>> Parameters)
+        { 
+            var client = new HttpClient();
+            client.BaseAddress  = new Uri(BaseAddress);
+            var req = new HttpRequestMessage(method, BaseAddress + RequestUri);
+            
+            foreach(var h in Headers)
+            {
+                client.DefaultRequestHeaders.TryAddWithoutValidation(h.Key, h.Value);
+               
+            }
+            if(Parameters != null)
+            req.Content = new FormUrlEncodedContent(Parameters);
+            
+            var response  = client.SendAsync(req).Result;
+            var responseContent = response.Content.ReadAsStringAsync().Result;
+            return responseContent;
+
+            
+        }
+    }
+}
