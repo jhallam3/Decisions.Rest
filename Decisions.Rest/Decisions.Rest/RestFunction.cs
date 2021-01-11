3c1aeedef2ccb8daa9cb0ac5bd99cd84715d60b8
Jon <Jon@WIN-B2L934R07O3>
2021-01-11 19:04:22 +0000

    try catch

--- Decisions.Rest/Decisions.Rest/RestFunction.cs
+++ Decisions.Rest/Decisions.Rest/RestFunction.cs
@@ -13,37 +13,46 @@ namespace Decisions.Rest
     {
         
         public string HttpRestRequestor(string method, string BaseAddress, string RequestUri, SimpleKeyValuePair[] Headers, SimpleKeyValuePair[] Parameters)
-        { 
-            if(method != "Post")
+        {
+            try
             {
-                throw new NotImplementedException();
+                if (method != "Post")
+                {
+                    throw new NotImplementedException();
+                }
+
+                var client = new HttpClient();
+                client.BaseAddress = new Uri(BaseAddress);
+                var _method = HttpMethod.Post;
+                var req = new HttpRequestMessage(_method, BaseAddress + RequestUri);
+
+                foreach (var h in Headers)
+                {
+                    client.DefaultRequestHeaders.TryAddWithoutValidation(h.Key, h.Value);
+
+                }
+                if (Parameters != null)
+                {
+                    List<KeyValuePair<string, string>> LKVP = new List<KeyValuePair<string, string>>();
+                    foreach (var items in Parameters)
+                    {
+                        LKVP.Add(new KeyValuePair<string, string>(items.Key, items.Value));
+                    }
+                    req.Content = new FormUrlEncodedContent(LKVP.ToArray());
+                }
+
+
+                var response = client.SendAsync(req).Result;
+                var responseContent = response.Content.ReadAsStringAsync().Result;
+                return responseContent;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
             }
            
-            var client = new HttpClient();
-            client.BaseAddress  = new Uri(BaseAddress);
-            var _method = HttpMethod.Post;
-            var req = new HttpRequestMessage(_method, BaseAddress + RequestUri);
-            
-            foreach(var h in Headers)
-            {
-                client.DefaultRequestHeaders.TryAddWithoutValidation(h.Key, h.Value);
-               
-            }
-            if (Parameters != null)
-            {
-                List<KeyValuePair<string, string>> LKVP = new List<KeyValuePair<string, string>>();
-                foreach (var items in Parameters)
-                {
-                    LKVP.Add(new KeyValuePair<string, string>(items.Key, items.Value));
-                }
-                req.Content = new FormUrlEncodedContent(LKVP.ToArray());
-            }
-            
-            
-            var response  = client.SendAsync(req).Result;
-            var responseContent = response.Content.ReadAsStringAsync().Result;
-            return responseContent;
-
             
         }
     }
